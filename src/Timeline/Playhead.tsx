33514646472c21adb7a62ab378921e86c4679fe6
LeoCantThinkofAName <lctoan.com@gmail.com>
2024-09-01 15:45:14 +0800

    feat: playhead behavior

--- src/Timeline/Playhead.tsx
+++ src/Timeline/Playhead.tsx
@@ -1,13 +1,15 @@
 type PlayheadProps = {
   time: number;
+  offset: number;
 };
 
-export const Playhead = ({ time }: PlayheadProps) => {
+export const Playhead = ({ time, offset }: PlayheadProps) => {
   return (
     <div
+      hidden={offset > time}
       className="absolute left-[316px] h-full border-l-2 border-solid border-yellow-600 z-10"
       data-testid="playhead"
-      style={{ transform: `translateX(calc(${time}px - 50%))` }}
+      style={{ transform: `translateX(calc(${time - offset}px - 50%))` }}
     >
       <div className="absolute border-solid border-[5px] border-transparent border-t-yellow-600 -translate-x-1.5" />
     </div>
--- src/Timeline/Timeline.tsx
+++ src/Timeline/Timeline.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { KeyframeList } from "./KeyframeList";
 import { PlayControls } from "./PlayControls";
@@ -10,6 +10,7 @@ import { useSyncScroll } from "../hooks/useSyncScroll";
 
 export const Timeline = () => {
   // FIXME: performance concerned
+  const [offset, setOffset] = useState(0);
   const [time, setTime] = useState(0);
   const [duration, setDuration] = useState(TIMELINE_MAX_DURATION);
   const rulerRef = useRef<HTMLDivElement>(null);
@@ -18,10 +19,17 @@ export const Timeline = () => {
   useSyncScroll([rulerRef, keyframeRef], "X");
   useSyncScroll([keyframeRef, trackListRef], "Y");
 
+  useEffect(() => {
+    rulerRef.current?.addEventListener("scroll", (e) => {
+      const currElm = e.target as HTMLDivElement;
+      setOffset(currElm.scrollLeft);
+    });
+  }, []);
+
   return (
     <div
       className="relative h-[300px] w-full grid grid-cols-[300px_1fr] grid-rows-[40px_1fr] 
-    bg-gray-800 border-t-2 border-solid border-gray-700"
+    bg-gray-800 border-t-2 border-solid border-gray-700 overflow-hidden"
       data-testid="timeline"
     >
       <PlayControls
@@ -33,7 +41,7 @@ export const Timeline = () => {
       <Ruler ref={rulerRef} duration={duration} setTime={setTime} time={time} />
       <TrackList ref={trackListRef} />
       <KeyframeList ref={keyframeRef} duration={duration} />
-      <Playhead time={time} />
+      <Playhead time={time} offset={offset} />
     </div>
   );
 };
